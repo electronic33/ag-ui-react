2c1f6296ab7837816372882dffb66999777d9093
Mátyás Fürtös <matyas.furtos@gmail.com>
2021-03-20 21:30:36 +0200

    lerna init

---
+++ packages/Label/stories/label.stories.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Label } from "../src";
+
+export default {
+  title: "LERNA/Label",
+  component: Label,
+};
+
+export const Default = (props) => <Label {...props}>Label</Label>;
