00e19c35ca216755dad3bde4cac87e2c8d7d1697
Mátyás Fürtös <matyas.furtos@gmail.com>
2021-03-20 10:46:54 +0200

    test-publish fixes

--- src/components/ButtonSpinner/ButtonSpinner.tsx
+++ src/components/ButtonSpinner/ButtonSpinner.tsx
@@ -8,11 +8,7 @@ export interface SpinnerProps {
   className?: string;
 }
 
-const ButtonSpinner: FC<SpinnerProps> = ({
-  className,
-  stroke = "rgba(59, 130, 246, 1)",
-  fill = "rgba(59, 130, 246, 1)",
-}) => {
+const ButtonSpinner: FC<SpinnerProps> = ({ className }: SpinnerProps) => {
   return (
     <svg
       className={classNames("btn-spinner", className)}
@@ -25,12 +21,12 @@ const ButtonSpinner: FC<SpinnerProps> = ({
         cx="12"
         cy="12"
         r="10"
-        stroke={`${stroke}`}
+        stroke="rgba(59, 130, 246, 1)"
         strokeWidth="4"
       />
       <path
         className="opacity-75"
-        fill={`${fill}`}
+        fill="rgba(59, 130, 246, 1)"
         d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
       />
     </svg>
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,1 @@
-import "./styles/index.css";
-
 export { default as HelloWorld } from "./components/Button/Button";
