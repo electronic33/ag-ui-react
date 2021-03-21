58d8ecc0082c95107eea47fda9807f1b095ca1fa
Mátyás Fürtös <matyas.furtos@gmail.com>
2021-03-21 11:18:45 +0200

    Monorepo setup

--- rollup.config.js
+++ rollup.config.js
@@ -4,27 +4,25 @@ import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import typescript from "rollup-plugin-typescript2";
 import del from "rollup-plugin-delete";
 
-import packageJson from "./package.json";
-
-export default {
-  input: "./src/index.ts",
+export default (input = "./index.ts") => ({
+  input,
   output: [
     {
-      file: packageJson.main,
+      file: "dist/index.js",
       format: "cjs",
       sourcemap: true,
     },
     {
-      file: packageJson.module,
+      file: "dist/index.es.js",
       format: "esm",
       sourcemap: true,
     },
   ],
   plugins: [
-    del({ targets: "build/*" }),
+    del({ targets: "dist/*" }),
     peerDepsExternal(),
     resolve(),
     commonjs(),
     typescript(),
   ],
-};
+});
