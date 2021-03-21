e8adf09707c78c6c81c9b1bf9a6c1c9e4b65230e
Mátyás Fürtös <matyas.furtos@gmail.com>
2021-03-21 19:10:56 +0200

    added text input

---
+++ packages/text-input/stories/text-input.stories.tsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import { MdLabelOutline } from "react-icons/md";
+import TextInput from "../src/text-input";
+
+export default {
+  title: "LERNA/TextInput",
+  component: TextInput,
+};
+
+export const Default = (): React.ReactNode => {
+  const [value, setValue] = useState("");
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+  return (
+    <TextInput
+      label="Textinput"
+      placeholder="This is a textinput"
+      value={value}
+      onChange={(event) => handleChange(event)}
+    />
+  );
+};
+
+export const WithIcon = (): React.ReactNode => (
+  <TextInput
+    label="Textinput"
+    Icon={MdLabelOutline}
+    placeholder="This is a textinput"
+    value=""
+  />
+);
+
+export const Error = (): React.ReactNode => (
+  <TextInput
+    label="Textinput"
+    Icon={MdLabelOutline}
+    placeholder="This is a textinput"
+    error="Error"
+  />
+);
+
+export const ErrorInLabel = (): React.ReactNode => (
+  <TextInput
+    label="Textinput"
+    Icon={MdLabelOutline}
+    placeholder="This is a textinput"
+    error="Error"
+    errorInLabel
+  />
+);
