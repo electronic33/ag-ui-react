a2b52bf73afa29d5001814129e2a6d2aeae77049
Mátyás Fürtös <matyas.furtos@gmail.com>
2021-03-22 11:29:53 +0200

    upgraded postcss to 8

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,6 +3,14 @@ module.exports = {
   addons: [
     "@storybook/addon-essentials",
     "@storybook/addon-a11y",
+    {
+      name: "@storybook/addon-postcss",
+      options: {
+        postcssLoaderOptions: {
+          implementation: require('postcss'),
+        }
+      },
+    }
   ],
   typescript: {
     check: false,
@@ -19,90 +27,59 @@ module.exports = {
   //     module: {
   //       ...config.module,
   //       rules: [
-  //         // Filter out the default .css rule.
-  //         ...config.module.rules.filter((rule) => /\.css$/ !== rule.test),
-  //         // Add our own css rule which in turn will read the postcss.config.js from project root.
-  //         {
-  //           test: /\.css1$/,
-  //           exclude: [/\.module\.css$/, /@storybook/],
-  //           use: [
-  //             "style-loader",
-  //             {
-  //               loader: "css-loader",
-  //               options: { importLoaders: 1, sourceMap: false },
-  //             },
-  //             {
-  //               loader: "postcss-loader",
-  //               options: {
-  //                 ident: "postcss",
-  //                 sourceMap: false,
-  //               },
-  //             },
-  //           ],
-  //         },
+  //         // Filter out the default .css and .module.css rules and replace them with our own.
+  //         ...(config.module.rules = config.module.rules.map((f) => {
+  //           if (f.oneOf === undefined) {
+  //             return f
+  //           }
+
+  //           return {
+  //             oneOf: f.oneOf.map((r) => {
+  //               if (r.test === undefined) {
+  //                 return r
+  //               }
+
+  //               if (r.test.toString() === '/\\.css$/' || r.test.toString() === '/\\.js$/') {
+  //                 return {
+  //                   test: /(\.css|\.js)$/,
+  //                   exclude: [/\.module\.css$/, /@storybook/],
+  //                   include: path.resolve(__dirname, "../"),
+  //                   use: [
+  //                     'style-loader',
+  //                     {
+  //                       loader: 'css-loader',
+  //                       options: { importLoaders: 1, sourceMap: false },
+  //                     },
+  //                     { loader: 'postcss-loader', options: {
+  //                       postcssOptions: {
+  //                         parser: "postcss-js",
+  //                       },
+  //                       execute: true,
+  //                     } },
+  //                   ],
+  //                 }
+  //               }
+  //               if (r.test.toString() === '/\\.module\\.css$/') {
+  //                 return {
+  //                   test: /\.module\.css$/,
+  //                   exclude: [/@storybook/],
+  //                   include: path.resolve(__dirname, "../"),
+  //                   use: [
+  //                     'style-loader',
+  //                     {
+  //                       loader: 'css-loader',
+  //                       options: { importLoaders: 1, sourceMap: false, modules: true },
+  //                     },
+  //                     'postcss-loader',
+  //                   ],
+  //                 }
+  //               }
+  //               return r
+  //             }),
+  //           }
+  //         })),
   //       ],
   //     },
-  //   };
+  //   }
   // },
-  webpackFinal: (config) => {
-    return {
-      ...config,
-      module: {
-        ...config.module,
-        rules: [
-          // Filter out the default .css and .module.css rules and replace them with our own.
-          ...(config.module.rules = config.module.rules.map((f) => {
-            if (f.oneOf === undefined) {
-              return f
-            }
-
-            return {
-              oneOf: f.oneOf.map((r) => {
-                if (r.test === undefined) {
-                  return r
-                }
-
-                if (r.test.toString() === '/\\.css$/' || r.test.toString() === '/\\.js$/') {
-                  return {
-                    test: /(\.css|\.js)$/,
-                    exclude: [/\.module\.css$/, /@storybook/],
-                    include: path.resolve(__dirname, "../"),
-                    use: [
-                      'style-loader',
-                      {
-                        loader: 'css-loader',
-                        options: { importLoaders: 1, sourceMap: false },
-                      },
-                      { loader: 'postcss-loader', options: {
-                        postcssOptions: {
-                          parser: "postcss-js",
-                        },
-                        execute: true,
-                      } },
-                    ],
-                  }
-                }
-                if (r.test.toString() === '/\\.module\\.css$/') {
-                  return {
-                    test: /\.module\.css$/,
-                    exclude: [/@storybook/],
-                    include: path.resolve(__dirname, "../"),
-                    use: [
-                      'style-loader',
-                      {
-                        loader: 'css-loader',
-                        options: { importLoaders: 1, sourceMap: false, modules: true },
-                      },
-                      'postcss-loader',
-                    ],
-                  }
-                }
-                return r
-              }),
-            }
-          })),
-        ],
-      },
-    }
-  },
 };
