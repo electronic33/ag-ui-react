a0579d5aaf619697974a8d27300ebe3a23e38d57
Mátyás Fürtös <matyas.furtos@gmail.com>
2021-03-25 10:46:07 +0200

    Custom Select fixes

--- packages/custom-select/src/custom-select.tsx
+++ packages/custom-select/src/custom-select.tsx
@@ -18,7 +18,7 @@ type SelectTypes = {
    The selected value.
   */
   selected: number | string;
-  onChange?: (item?) => void;
+  onChange?: (item?: number | string) => void;
   containerClassName: string;
   cursorPointer?: boolean;
   label: string;
@@ -29,13 +29,13 @@ type SelectTypes = {
   loadingText?: string;
 };
 
-function getNextItemFromSearch<T>(
+const getNextItemFromSearch = <T,>(
   items: T[],
   searchString: string,
   itemToString: (item: T) => string,
   currentItem: T,
-) {
-  if (searchString == null) {
+) => {
+  if (searchString === null) {
     return currentItem;
   }
 
@@ -44,6 +44,7 @@ function getNextItemFromSearch<T>(
     const foundItem = items.find((item) =>
       itemToString(item).toLowerCase().startsWith(searchString.toLowerCase()),
     );
+
     return foundItem;
   }
 
@@ -60,19 +61,23 @@ function getNextItemFromSearch<T>(
     if (matchingItems.includes(currentItem)) {
       const currentIndex = matchingItems.indexOf(currentItem);
       nextIndex = currentIndex + 1;
+
       if (nextIndex === matchingItems.length) {
         nextIndex = 0;
       }
+
       return matchingItems[nextIndex];
     }
+
     // Else, we pick the first item in the available items
     nextIndex = items.indexOf(matchingItems[0]);
+
     return items[nextIndex];
   }
 
   // a decent fallback to the currentItem
   return currentItem;
-}
+};
 
 export const Select = ({
   options,
@@ -88,10 +93,10 @@ export const Select = ({
   loadingText,
 }: SelectTypes): React.ReactElement => {
   const [isOpen, setIsOpen] = useState(false);
-  const [activeIndex, setActiveIndex] = useState(null);
+  const [activeIndex, setActiveIndex] = useState<number>();
 
-  const selectOptionsRef = useRef();
-  const selectButtonRef = useRef();
+  const selectOptionsRef = useRef<HTMLDivElement>();
+  const selectButtonRef = useRef<HTMLButtonElement>();
   const selectedOption = useMemo(
     () =>
       !isLoading
@@ -99,6 +104,7 @@ export const Select = ({
         : { label: "No selected option", Icon: undefined, value: "" },
     [selected, isLoading, options],
   );
+
   useEffect(() => {
     function handleClickOutside(event) {
       if (
@@ -116,59 +122,22 @@ export const Select = ({
     return () => {
       document.removeEventListener("mousedown", handleClickOutside);
     };
-  }, [selectOptionsRef, isOpen]);
+  }, [isOpen]);
 
   useEffect(() => {
-    const handleKeydown = (e) => {
-      if (isOpen && e.code === "Escape") {
+    const handleKeydown = (event: KeyboardEvent) => {
+      if (isOpen && event.code === "Escape") {
         setIsOpen(false);
       }
 
-      if (
-        e.code === "ArrowDown" &&
-        !isOpen &&
-        document.activeElement === selectButtonRef.current
-      ) {
-        setIsOpen(true);
-        setActiveIndex(0);
-      }
-      if (
-        e.code === "ArrowUp" &&
-        !isOpen &&
-        document.activeElement === selectButtonRef.current
-      ) {
-        setIsOpen(true);
-        setActiveIndex(options.length - 1);
-      }
-      if (activeIndex === undefined && e.code === "ArrowDown") {
-        setActiveIndex(0);
-      }
-      if (activeIndex === undefined && e.code === "ArrowUp") {
-        setActiveIndex(options.length - 1);
-      }
+      if (event.code === "ArrowDown") {
+        if (!isOpen && document.activeElement === selectButtonRef.current) {
+          setIsOpen(true);
+          setActiveIndex(0);
+        } else if (activeIndex === undefined) {
+          setActiveIndex(0);
+        }
 
-      const indexWithStartingLetter = options.findIndex(
-        (element) => element.label.charAt(0).toLowerCase() === e.key,
-      );
-      if (indexWithStartingLetter !== -1) {
-        setActiveIndex(indexWithStartingLetter);
-      }
-
-      const nextItem = getNextItemFromSearch(
-        options.map((element) => element.label),
-        e.key,
-        (thing) => thing,
-        options[activeIndex].label,
-      );
-
-      if (nextItem) {
-        const index = options.findIndex(
-          (element) => element.label === nextItem,
-        );
-        setActiveIndex(index);
-      }
-
-      if (isOpen && e.code === "ArrowDown") {
         if (options.length > 0) {
           if (options.length - 1 === activeIndex) {
             setActiveIndex(0);
@@ -178,7 +147,14 @@ export const Select = ({
         }
       }
 
-      if (isOpen && e.code === "ArrowUp") {
+      if (event.code === "ArrowUp") {
+        if (!isOpen && document.activeElement === selectButtonRef.current) {
+          setIsOpen(true);
+          setActiveIndex(options.length - 1);
+        } else if (activeIndex === undefined) {
+          setActiveIndex(options.length - 1);
+        }
+
         if (options.length > 0) {
           if (activeIndex === 0) {
             setActiveIndex(options.length - 1);
@@ -188,7 +164,30 @@ export const Select = ({
         }
       }
 
-      if ((isOpen && e.code === "Space") || (isOpen && e.code === "Enter")) {
+      // const indexWithStartingLetter = options.findIndex(
+      //   (element) => element.label.charAt(0).toLowerCase() === event.key,
+      // );
+
+      // if (indexWithStartingLetter !== -1) {
+      //   setActiveIndex(indexWithStartingLetter);
+      // }
+
+      const nextItem = getNextItemFromSearch(
+        options.map((element) => element.label),
+        event.key,
+        (thing) => thing,
+        options[activeIndex].label,
+      );
+
+      if (nextItem) {
+        const index = options.findIndex(
+          (element) => element.label === nextItem,
+        );
+
+        setActiveIndex(index);
+      }
+
+      if ((event.code === "Space" || event.code === "Enter") && isOpen) {
         onChange(options[activeIndex].value);
         setIsOpen(false);
       }
@@ -206,8 +205,6 @@ export const Select = ({
     enter: { opacity: 1, transform: "translateY(10px)" },
     leave: { opacity: 0, transform: "translateY(0px)" },
   });
-
-  if (!options) return <h1>csumi</h1>;
 
   return (
     <FocusLock restoreFocus isDisabled={!isOpen}>
@@ -310,15 +307,21 @@ export const Select = ({
                   {!isLoading &&
                     options.map((option, index) => (
                       <div
+                        role="button"
+                        tabIndex={-1}
                         className="w-full"
                         onMouseEnter={() => setActiveIndex(index)}
                         onMouseLeave={() => setActiveIndex(undefined)}
-                        onClick={() =>
-                          onChange(option.value) || setIsOpen(false)
-                        }
+                        onClick={() => {
+                          onChange(option.value);
+                          setIsOpen(false);
+                        }}
+                        onKeyDown={() => {
+                          // TODO
+                        }}
                         key={option.value}
-                        // value={option.value}
                       >
+                        {/* TODO refactor to use classnames */}
                         <div
                           className={`${
                             activeIndex === index
