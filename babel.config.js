d46853cc7e0e5c7fdbd91e19e04db40cb0ae9067
Mátyás Fürtös <matyas.furtos@gmail.com>
2021-03-25 12:16:35 +0200

    added commitlint

--- babel.config.js
+++ babel.config.js
@@ -1,31 +1,31 @@
 module.exports = {
-  "presets": [
+  presets: [
     [
       "@babel/preset-env",
       {
-        "targets": {
-          "node": "current"
-        }
-      }
+        targets: {
+          node: "current",
+        },
+      },
     ],
     "@babel/preset-react",
-    "@babel/preset-typescript"
+    "@babel/preset-typescript",
   ],
-  "plugins": [
+  plugins: [
     "@babel/plugin-transform-runtime",
     "@babel/plugin-syntax-dynamic-import",
     "@babel/plugin-transform-modules-commonjs",
     "@babel/plugin-proposal-class-properties",
     "@babel/plugin-proposal-optional-chaining",
-    "@babel/plugin-proposal-nullish-coalescing-operator"
+    "@babel/plugin-proposal-nullish-coalescing-operator",
   ],
-  "env": {
-    "test": {
-      "plugins": [
+  env: {
+    test: {
+      plugins: [
         "transform-es2015-modules-commonjs",
-        "@babel/plugin-proposal-class-properties"
+        "@babel/plugin-proposal-class-properties",
       ],
-      "presets": ["@babel/preset-react"]
-    }
-  }
-}
+      presets: ["@babel/preset-react"],
+    },
+  },
+};
