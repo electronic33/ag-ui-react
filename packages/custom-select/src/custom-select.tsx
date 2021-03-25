3297ea1a25e3945e0be19c71e8f3de974882a9d1
Toth Levente <leventetoth9@gmail.com>
2021-03-25 10:12:12 +0200

    custom-select

--- packages/custom-select/src/custom-select.tsx
+++ packages/custom-select/src/custom-select.tsx
@@ -1,146 +1,362 @@
-import React from "react";
+import React, { useEffect, useMemo, useRef, useState } from "react";
 import classNames from "classnames";
-import { Label } from "@app-garage/label";
+import { ButtonSpinner } from "@app-garage/button-spinner";
+import { Button } from "@app-garage/button";
+import { FocusLock } from "@app-garage/focus-trap";
+import { useTransition, animated } from "react-spring";
 
-type FieldInputProps = {
-  value: string;
-  name: string;
-  multiple?: boolean;
-  checked?: boolean;
-  onChange: {
-    (e: React.ChangeEvent<any>): void;
-    <T = string | React.ChangeEvent<any>>(
-      field: T,
-    ): T extends React.ChangeEvent<any>
-      ? void
-      : (e: string | React.ChangeEvent<any>) => void;
-  };
-
-  onBlur: {
-    (e: React.FocusEvent<any>): void;
-    <T = string | any>(fieldOrEvent: T): T extends string
-      ? (e: any) => void
-      : void;
-  };
-};
-
-type FormikProps = {
-  touched?: Record<string, string>;
-  errors?: Record<string, string>;
-};
-
-type TextInputProps = {
-  id?: string;
-  label?: string;
-  secondaryLabel?: string;
-  Icon?: React.ComponentType<{ className: string }>;
-  required?: boolean;
-  name?: string;
-  value?: string | number;
-  onChange?: (event) => void | (() => void);
-  onClick?: (event) => void;
-  onKeyDown?: (event) => void;
+type SelectTypes = {
+  /**
+   Array of option objects, each has a label and a value, and optionally an Icon.
+  */
+  options: {
+    label: string;
+    value: number | string;
+    Icon?: React.ComponentType<{ className?: string }>;
+  }[];
+  /**
+   The selected value.
+  */
+  selected: number | string;
+  onChange?: (item?) => void;
+  containerClassName: string;
+  cursorPointer?: boolean;
+  label: string;
+  isLoading?: boolean;
+  spinnerClassName?: string;
   error?: string;
-  containerClassName?: string;
-  inputClassName?: string;
-  type?: string;
-  max?: string | number;
-  showMax?: boolean;
-  placeholder?: string;
-  field?: FieldInputProps;
-  form?: FormikProps;
-  onBlur?: () => void;
-  errorInLabel?: boolean;
-  disabled?: boolean;
+  retryFn?: () => void;
+  loadingText?: string;
 };
 
-export const TextInput = ({
-  id,
-  label = "",
-  secondaryLabel = "",
-  Icon,
-  required = false,
-  name = "",
-  value = "",
-  onChange = undefined,
-  onClick = undefined,
-  onKeyDown = undefined,
-  error = "",
-  containerClassName,
-  inputClassName,
-  type = "text",
-  max = undefined,
-  showMax = false,
-  placeholder,
-  field,
-  form,
-  onBlur = undefined,
-  errorInLabel = false,
-  disabled = false,
-}: TextInputProps) => (
-  <div className={classNames("flex flex-col relative", containerClassName)}>
-    {label && (
-      <Label
-        secondaryText={secondaryLabel}
-        required={required}
-        errorText={
-          errorInLabel
-            ? (form?.touched?.[field?.name] && form?.errors?.[field?.name]) ||
-              error ||
-              ""
-            : undefined
-        }
-        htmlFor={field?.name || name ? field?.name || name : undefined}
-      >
-        {Icon ? <Icon className="w-5 mr-2" /> : null}
-        {label}
-      </Label>
-    )}
-    <input
-      placeholder={placeholder}
-      type={type}
-      className={classNames(
-        {
-          "border-red-700":
-            Boolean(
-              form?.errors?.[field?.name] && form?.touched?.[field?.name],
-            ) || Boolean(error),
-          "bg-gray-200 cursor-not-allowed": disabled,
-        },
-        "text-input",
-        inputClassName,
-      )}
-      value={field?.value || value}
-      onChange={field?.onChange || onChange}
-      onClick={onClick}
-      onKeyDown={onKeyDown}
-      name={field?.name || name}
-      id={id || field?.name || name ? field?.name || name : undefined}
-      max={max}
-      onBlur={field?.onBlur || onBlur}
-      disabled={disabled}
-    />
+function getNextItemFromSearch<T>(
+  items: T[],
+  searchString: string,
+  itemToString: (item: T) => string,
+  currentItem: T,
+) {
+  if (searchString == null) {
+    return currentItem;
+  }
 
-    {showMax ? (
-      <p className="flex absolute right-0 bottom-0 text-gray-400 -mb-4 text-xs">
-        {`${
-          field?.value ? field?.value.length : (value as string).length
-        }/${max}`}
-      </p>
-    ) : null}
-    {!errorInLabel ? (
-      <>
-        {form?.errors?.[field?.name] && form?.touched?.[field?.name] ? (
-          <p className="flex absolute inset-x-0 bottom-0 text-red-700 -mb-4 text-xs">
-            {form?.errors?.[field?.name]}
-          </p>
-        ) : null}
-        {error ? (
-          <p className="flex absolute inset-x-0 bottom-0 text-red-700 -mb-4 text-xs">
-            {error}
-          </p>
-        ) : null}
-      </>
-    ) : null}
-  </div>
-);
+  // If current item doesn't exist, find the item that matches the search string
+  if (!currentItem) {
+    const foundItem = items.find((item) =>
+      itemToString(item).toLowerCase().startsWith(searchString.toLowerCase()),
+    );
+    return foundItem;
+  }
+
+  // Filter items for ones that match the search string (case insensitive)
+  const matchingItems = items.filter((item) =>
+    itemToString(item).toLowerCase().startsWith(searchString.toLowerCase()),
+  );
+
+  // If there's a match, let's get the next item to select
+  if (matchingItems.length > 0) {
+    let nextIndex: number;
+
+    // If the currentItem is in the available items, we move to the next available option
+    if (matchingItems.includes(currentItem)) {
+      const currentIndex = matchingItems.indexOf(currentItem);
+      nextIndex = currentIndex + 1;
+      if (nextIndex === matchingItems.length) {
+        nextIndex = 0;
+      }
+      return matchingItems[nextIndex];
+    }
+    // Else, we pick the first item in the available items
+    nextIndex = items.indexOf(matchingItems[0]);
+    return items[nextIndex];
+  }
+
+  // a decent fallback to the currentItem
+  return currentItem;
+}
+
+export const Select = ({
+  options,
+  selected,
+  onChange,
+  containerClassName,
+  label,
+  cursorPointer,
+  isLoading = false,
+  spinnerClassName,
+  error,
+  retryFn,
+  loadingText,
+}: SelectTypes): React.ReactElement => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  const selectOptionsRef = useRef();
+  const selectButtonRef = useRef();
+  const selectedOption = useMemo(
+    () =>
+      !isLoading
+        ? options?.find((option) => option.value === selected)
+        : { label: "No selected option", Icon: undefined, value: "" },
+    [selected, isLoading, options],
+  );
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (
+        isOpen &&
+        selectOptionsRef.current &&
+        !selectOptionsRef.current.contains(event.target) &&
+        selectButtonRef.current &&
+        !selectButtonRef.current.contains(event.target)
+      ) {
+        setIsOpen(false);
+      }
+    }
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [selectOptionsRef, isOpen]);
+
+  useEffect(() => {
+    const handleKeydown = (e) => {
+      if (isOpen && e.code === "Escape") {
+        setIsOpen(false);
+      }
+
+      if (
+        e.code === "ArrowDown" &&
+        !isOpen &&
+        document.activeElement === selectButtonRef.current
+      ) {
+        setIsOpen(true);
+        setActiveIndex(0);
+      }
+      if (
+        e.code === "ArrowUp" &&
+        !isOpen &&
+        document.activeElement === selectButtonRef.current
+      ) {
+        setIsOpen(true);
+        setActiveIndex(options.length - 1);
+      }
+      if (activeIndex === undefined && e.code === "ArrowDown") {
+        setActiveIndex(0);
+      }
+      if (activeIndex === undefined && e.code === "ArrowUp") {
+        setActiveIndex(options.length - 1);
+      }
+
+      const indexWithStartingLetter = options.findIndex(
+        (element) => element.label.charAt(0).toLowerCase() === e.key,
+      );
+      if (indexWithStartingLetter !== -1) {
+        setActiveIndex(indexWithStartingLetter);
+      }
+
+      const nextItem = getNextItemFromSearch(
+        options.map((element) => element.label),
+        e.key,
+        (thing) => thing,
+        options[activeIndex].label,
+      );
+
+      if (nextItem) {
+        const index = options.findIndex(
+          (element) => element.label === nextItem,
+        );
+        setActiveIndex(index);
+      }
+
+      if (isOpen && e.code === "ArrowDown") {
+        if (options.length > 0) {
+          if (options.length - 1 === activeIndex) {
+            setActiveIndex(0);
+          } else {
+            setActiveIndex((prev) => prev + 1);
+          }
+        }
+      }
+
+      if (isOpen && e.code === "ArrowUp") {
+        if (options.length > 0) {
+          if (activeIndex === 0) {
+            setActiveIndex(options.length - 1);
+          } else {
+            setActiveIndex((prev) => prev - 1);
+          }
+        }
+      }
+
+      if ((isOpen && e.code === "Space") || (isOpen && e.code === "Enter")) {
+        onChange(options[activeIndex].value);
+        setIsOpen(false);
+      }
+    };
+
+    window.addEventListener("keydown", handleKeydown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeydown);
+    };
+  }, [activeIndex, isOpen, options, onChange]);
+
+  const transitions = useTransition(isOpen, null, {
+    from: { opacity: 0, transform: "translateY(0px)" },
+    enter: { opacity: 1, transform: "translateY(10px)" },
+    leave: { opacity: 0, transform: "translateY(0px)" },
+  });
+
+  if (!options) return <h1>csumi</h1>;
+
+  return (
+    <FocusLock restoreFocus isDisabled={!isOpen}>
+      <div className={containerClassName}>
+        <>
+          {label && (
+            <div className="block text-sm leading-5 font-medium text-gray-700 mb-2">
+              <p className={classNames("", { "text-red-600": error })}>
+                {label}
+              </p>
+            </div>
+          )}
+          <div className="relative">
+            <button
+              type="button"
+              ref={selectButtonRef}
+              onClick={() => {
+                if (transitions.length === 1) {
+                  setIsOpen((prev) => {
+                    setActiveIndex(
+                      options.findIndex(
+                        (element) => element.value === selectedOption.value,
+                      ),
+                    );
+
+                    return !prev;
+                  });
+                }
+              }}
+              className="inline-flex w-full rounded-md shadow-sm focus:ring-2 focus:ring-blue-500"
+            >
+              <div
+                className={`flex items-center ${
+                  cursorPointer ? "cursor-pointer" : "cursor-default"
+                } ${isLoading ? "justify-between" : "justify-start"} ${
+                  error
+                    ? "border border-red-600 justify-between pr-2"
+                    : "border border-gray-300 focus:border-blue-300 pr-8"
+                } relative w-full rounded-md border border-gray-300 bg-white pl-2 py-2 text-left focus:outline-none focus:shadow-outline-blue  transition ease-in-out duration-150 sm:text-sm sm:leading-5`}
+              >
+                {isLoading ? (
+                  <ButtonSpinner
+                    className={classNames("flex-shrink-0", spinnerClassName)}
+                  />
+                ) : (
+                  <>
+                    {error ? (
+                      <p className="text-red-600 ">{error}</p>
+                    ) : (
+                      <>
+                        {selectedOption?.Icon && (
+                          <span className="flex items-center mr-1.5">
+                            {selectedOption?.Icon}
+                          </span>
+                        )}
+                        <span className="block truncate">
+                          {selectedOption?.label}
+                        </span>
+                        <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
+                          <svg
+                            className="h-5 w-5 text-gray-400"
+                            viewBox="0 0 20 20"
+                            fill="none"
+                            stroke="currentColor"
+                          >
+                            <path
+                              d="M7 7l3-3 3 3m0 6l-3 3-3-3"
+                              strokeWidth="1.5"
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                            />
+                          </svg>
+                        </span>{" "}
+                      </>
+                    )}
+                  </>
+                )}
+                {isLoading && loadingText && (
+                  <p className=" text-gray-400">{loadingText}</p>
+                )}
+                {error && retryFn && (
+                  <Button
+                    className="text-white font-semibold bg-red-400 hover:bg-red-500 text-base focus:bg-red-500 px-2 py-1 rounded"
+                    onClick={retryFn}
+                  >
+                    Try again
+                  </Button>
+                )}
+              </div>
+            </button>
+
+            {transitions.map(({ item, key, props }) =>
+              item && !error && !isLoading ? (
+                <animated.div
+                  key={key}
+                  style={props}
+                  ref={selectOptionsRef}
+                  className="max-h-60 rounded-md py-1 text-base leading-6 shadow-lg overflow-auto focus:outline-none sm:text-sm sm:leading-5"
+                >
+                  {!isLoading &&
+                    options.map((option, index) => (
+                      <div
+                        className="w-full"
+                        onMouseEnter={() => setActiveIndex(index)}
+                        onMouseLeave={() => setActiveIndex(undefined)}
+                        onClick={() =>
+                          onChange(option.value) || setIsOpen(false)
+                        }
+                        key={option.value}
+                        // value={option.value}
+                      >
+                        <div
+                          className={`${
+                            activeIndex === index
+                              ? "text-white bg-blue-600"
+                              : "text-gray-900"
+                          } ${
+                            cursorPointer ? "cursor-pointer" : "cursor-default"
+                          } select-none relative py-2 px-2 flex items-center`}
+                        >
+                          {option.Icon && (
+                            <span
+                              className={`${
+                                activeIndex === index
+                                  ? "text-white"
+                                  : "text-blue-600"
+                              } flex items-center mr-1.5`}
+                            >
+                              {option.Icon}
+                            </span>
+                          )}
+                          <span
+                            className={`${
+                              option.value === selectedOption.value
+                                ? "font-semibold"
+                                : "font-normal"
+                            } block truncate`}
+                          >
+                            {option.label}
+                          </span>
+                        </div>
+                      </div>
+                    ))}
+                </animated.div>
+              ) : null,
+            )}
+          </div>
+        </>
+      </div>
+    </FocusLock>
+  );
+};
--- packages/custom-select/stories/custom-select.stories.tsx
+++ packages/custom-select/stories/custom-select.stories.tsx
@@ -1,52 +1,27 @@
 import React, { useState } from "react";
-import { MdLabelOutline } from "react-icons/md";
-import { TextInput } from "../src/custom-select";
+import { Select } from "../src";
 
 export default {
-  title: "LERNA/CustomSelect",
-  component: TextInput,
+  title: "FORMS/CustomSelect",
+  component: Select,
 };
 
-export const Default = (): React.ReactNode => {
-  const [value, setValue] = useState("");
-
-  const handleChange = (event) => {
-    setValue(event.target.value);
-  };
+export const Default = (props): React.ReactNode => {
+  const [value, setValue] = useState(0);
   return (
-    <TextInput
-      label="Textinput"
-      placeholder="This is a textinput"
-      value={value}
-      onChange={(event) => handleChange(event)}
+    <Select
+      className="max-w-sm w-64 mb-5 my-2 mr-2"
+      onChange={setValue}
+      selected={value}
+      label="Select"
+      {...props}
+      options={[
+        { label: "All", value: 0 },
+        { label: "Option 1", value: 1 },
+        { label: "Option 2", value: 2 },
+        { label: "Option 3", value: 3 },
+      ]}
+      // {...props} makes the option property undefined
     />
   );
 };
-
-export const WithIcon = (): React.ReactNode => (
-  <TextInput
-    label="Textinput"
-    Icon={MdLabelOutline}
-    placeholder="This is a textinput"
-    value=""
-  />
-);
-
-export const Error = (): React.ReactNode => (
-  <TextInput
-    label="Textinput"
-    Icon={MdLabelOutline}
-    placeholder="This is a textinput"
-    error="Error"
-  />
-);
-
-export const ErrorInLabel = (): React.ReactNode => (
-  <TextInput
-    label="Textinput"
-    Icon={MdLabelOutline}
-    placeholder="This is a textinput"
-    error="Error"
-    errorInLabel
-  />
-);
