3d46fe6a9b94a7554d180513948c9d900abd7780
Mátyás Fürtös <matyas.furtos@gmail.com>
2021-06-27 22:31:02 +0300

    test: 💍 input padding

--- tw-plugins/core/src/classes/text-input.js
+++ tw-plugins/core/src/classes/text-input.js
@@ -3,7 +3,7 @@ const TextInputBaseClasses = (theme) => ({
     borderRadius: theme('borderRadius.md'),
     borderWidth: theme('borderWidth.2'),
     borderColor: theme('color.gray.200'),
-    padding: theme('spacing.2'),
+    padding: theme('input.padding') || theme('spacing.2'),
     // height: theme('spacing.10'),
     paddingLeft: theme('spacing.3'),
     paddingRight: theme('spacing.3'),
